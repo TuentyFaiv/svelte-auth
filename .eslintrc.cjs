3a6a8e74451e2ecd353202b45d6f750a9a87666c
Tonalli López <contact@tuentyfaiv.com>
2024-02-21 21:46:40 -0600

    🚧 REFACTOR: Update dependencies and eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,41 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ["airbnb-base", "airbnb-typescript/base", 'plugin:@typescript-eslint/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	extends: [
+		"eslint:recommended",
+		"airbnb-base",
+		"airbnb-typescript/base",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"turbo"
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
-		project: "./tsconfig.json"
+		extraFileExtensions: [".svelte"],
+		project: "./tsconfig.json",
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser"
+			}
+		}
+	],
 	rules: {
 		"import/extensions": 0,
 		"import/no-extraneous-dependencies": 0,
 		"import/no-unresolved": 0,
 		"import/prefer-default-export": 0,
-		"linebreak-style": 0,
-		"quotes": "off",
-		"@typescript-eslint/quotes": [
-			"error",
-			"double"
-		],
-		"comma-dangle": "off",
-		"@typescript-eslint/comma-dangle": [
-			"error",
-			"never"
-		],
-		"max-len": [
-			"error",
-			{
-				"code": 130
-			}
-		],
 		"dot-notation": "off",
 		"@typescript-eslint/dot-notation": 0,
 		"padded-blocks": 0,
@@ -58,12 +54,29 @@ module.exports = {
 				"format": ["camelCase", "UPPER_CASE", "snake_case"],
 			}
 		],
+		"linebreak-style": 0,
+		"quotes": "off",
+		"@typescript-eslint/quotes": [
+			"error",
+			"double"
+		],
+		"comma-dangle": "off",
+		"@typescript-eslint/comma-dangle": [
+			"error",
+			"always-multiline"
+		],
+		"max-len": [
+			"error",
+			{
+				"code": 130
+			}
+		],
 		"object-shorthand": 0,
 		"object-curly-newline": 0,
-		"no-useless-return": 0,
-		"no-case-declarations": 0,
 		"no-unused-vars": "off",
 		"@typescript-eslint/no-unused-vars": 1,
+		"no-useless-return": 0,
+		"no-case-declarations": 0,
 		"no-debugger": 0,
 		"no-plusplus": 0,
 		"no-tabs": ["error", { allowIndentationTabs: true }]
